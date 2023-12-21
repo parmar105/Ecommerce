7f5701455ff3ae2f375ed3d73a74d172f6bf84bf
parmar105 <hrithikparmar11@gmail.com>
2023-12-22 00:36:12 +0530

    Added Routes and ProductScreen Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main className='py-3'>
-        <div className='container'>
-          <HomeScreen />
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <main className="py-3">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Rating from "./Rating";
+import {Link} from 'react-router-dom'
 
 function Product(props) {
   const { product } = props;
   return (
     <div className="card my-3">
-      <img src={product.image} className="card-img-top" alt="..." />
+      <Link to={'/product/:id'}><img src={product.image} className="card-img-top" alt="..." /></Link>
       <div className="card-body">
-        <h5 className="card-title my-2">{product.name}</h5>
+      <Link to={'/product/:id'}><h5 className="card-title my-2">{product.name}</h5></Link>
         <p className="card-text">
           {/* {product.rating} from {product.numReviews} reviews */}
           <Rating value={product.rating} text={`${product.numReviews} reviews`} color={`#f8e825`} />
