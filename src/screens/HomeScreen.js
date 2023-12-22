0698bed6b714ce4b2d3e2a8230cadd5642b85373
parmar105 <hrithikparmar11@gmail.com>
2023-12-22 06:11:10 +0530

    updated homescreen added flex to center content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,13 +5,13 @@ import Product from "../components/Product";
 function HomeScreen() {
   console.log(products);
   return (
-    <div className="row">
-        <h1>LATEST PRODUCTS</h1>
-      {products.map((product)=>
+    <div className="row d-flex justify-content-center">
+      <h1>LATEST PRODUCTS</h1>
+      {products.map((product) => (
         <div key={product._id} className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-        <Product product={product}/>
+          <Product product={product} />
         </div>
-      )}
+      ))}
     </div>
   );
 }
